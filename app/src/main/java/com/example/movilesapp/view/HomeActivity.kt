250788af6dbde0f7876aacee8f8389e0eca46f8d
Michael <m.blnaquicett@uniandes.edu.co>
2023-09-22 22:55:05 -0500

    Created the calls with home, updated manifest for summary and setting.

--- app/src/main/java/com/example/movilesapp/view/HomeActivity.kt
+++ app/src/main/java/com/example/movilesapp/view/HomeActivity.kt
@@ -8,6 +8,7 @@ import com.example.movilesapp.databinding.ActivityHomeBinding
 class HomeActivity : AppCompatActivity() {
     private lateinit var binding: ActivityHomeBinding
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         binding = ActivityHomeBinding.inflate(layoutInflater)
         setContentView(binding.root)
@@ -15,6 +16,14 @@ class HomeActivity : AppCompatActivity() {
         binding.CardViewHistory.setOnClickListener {
             val intent = Intent(this, HistoryActivity::class.java)
             startActivity(intent)
+        }
+        binding.CardViewSetting.setOnClickListener {
+            val intentSe = Intent(this, SettingActivity::class.java)
+            startActivity(intentSe)
+        }
+        binding.CardViewSummary.setOnClickListener {
+            val intentSu = Intent( this, SummaryActivity::class.java)
+            startActivity(intentSu)
         }
     }
 }
